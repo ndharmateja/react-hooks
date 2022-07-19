5e1098287089dd01f4aa4364c71b19fec85e1c1e
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-19 15:22:10 +0530

    03 extra

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -3,11 +3,16 @@
 
 import * as React from 'react'
 
-function Name({name, onNameChange}) {
+function Name() {
+  const [name, setName] = React.useState('')
   return (
     <div>
       <label htmlFor="name">Name: </label>
-      <input id="name" value={name} onChange={onNameChange} />
+      <input
+        id="name"
+        value={name}
+        onChange={({target: {value}}) => setName(value)}
+      />
     </div>
   )
 }
@@ -30,26 +35,23 @@ function FavoriteAnimal({animal, onAnimalChange}) {
 
 // 💣 remove this component in favor of the new one
 function Display({name, animal}) {
-  return (
-    <div>{`Hey ${name}, you are great! Your favorite animal is ${animal}.`}</div>
-  )
+  return <div>{`Your favorite animal is ${animal}.`}</div>
 }
 
 function App() {
   // 🐨 add a useState for the animal
-  const [name, setName] = React.useState('')
   const [animal, setAnimal] = React.useState('')
 
   return (
     <form>
-      <Name name={name} onNameChange={event => setName(event.target.value)} />
+      <Name />
       {/* 🐨 pass the animal and onAnimalChange prop here (similar to the Name component above) */}
       <FavoriteAnimal
         animal={animal}
         onAnimalChange={({target: {value}}) => setAnimal(value)}
       />
       {/* 🐨 pass the animal prop here */}
-      <Display name={name} animal={animal} />
+      <Display animal={animal} />
     </form>
   )
 }
